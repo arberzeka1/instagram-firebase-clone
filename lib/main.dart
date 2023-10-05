fe73f32026c61029ea3a2fda8c5421e5c81e09a2
arberzeka1 <arberzeka1@gmail.com>
2023-10-05 11:13:13 +0200

    added configurations and functions for google and facebook login

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,63 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:instagram_clone/state/auth/backend/authenticator.dart';
 
-void main() {
-  runApp(const MyApp());
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const App());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class App extends StatelessWidget {
+  const App({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          brightness: Brightness.dark,
+          primarySwatch: Colors.blue,
+        ),
+        darkTheme: ThemeData(
+          brightness: Brightness.dark,
+          primarySwatch: Colors.blueGrey,
+          indicatorColor: Colors.blueGrey,
+        ),
+        themeMode: ThemeMode.dark,
+        home: const HomePage());
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home Page'),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      body: Column(
+        children: [
+          TextButton(
+              onPressed: () async {
+                final result = await Authenticator().loginWithGoogle();
+                print("REsult $result");
+              },
+              child: const Text('Sign In with Google')),
+          TextButton(
+              onPressed: () async {
+                final result = await Authenticator().logInWithFacebook();
+                print(result);
+              },
+              child: const Text('Sign In with Facebook')),
+        ],
+      ),
     );
   }
 }
---
+++ lib/state/auth/backend/authenticator.dart
@@ -0,0 +1,75 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:instagram_clone/state/auth/constants/constants.dart';
+import 'package:instagram_clone/state/auth/models/auth_result.dart';
+import 'package:instagram_clone/state/posts/typedefs/user_id.dart';
+
+class Authenticator {
+  // const Authenticator();
+  UserId? get userId => FirebaseAuth.instance.currentUser?.uid;
+  bool get isAlreadyLoggedIn => userId != null;
+  String get displayNAme =>
+      FirebaseAuth.instance.currentUser?.displayName ?? '';
+  String? get email => FirebaseAuth.instance.currentUser?.email ?? '';
+
+  Future<void> logOut() async {
+    await FirebaseAuth.instance.signOut();
+    await GoogleSignIn().signOut();
+    await FacebookAuth.instance.logOut();
+  }
+
+  Future<AuthResult> logInWithFacebook() async {
+    final logInResult = await FacebookAuth.instance.login();
+    final token = logInResult.accessToken?.token;
+    if (token == null) {
+      return AuthResult.aborted;
+    }
+    final oauthCredentials = FacebookAuthProvider.credential(token);
+    try {
+      await FirebaseAuth.instance.signInWithCredential(oauthCredentials);
+      return AuthResult.success;
+    } on FirebaseAuthException catch (e) {
+      final email = e.email;
+      final credential = e.credential;
+      if (e.code == Constants.accountExistsWithDifferentCredential &&
+          email != e.email &&
+          credential != null) {
+        final providers =
+            FirebaseAuth.instance.fetchSignInMethodsForEmail(email!).toString();
+        if (providers.contains(Constants.googleCom)) {
+          await loginWithGoogle();
+          FirebaseAuth.instance.currentUser?.linkWithCredential(credential);
+        }
+        return AuthResult.success;
+      }
+      return AuthResult.failure;
+    }
+  }
+
+  Future<AuthResult> loginWithGoogle() async {
+    print('HEREEE');
+    final GoogleSignIn googleSignIn = GoogleSignIn(
+      scopes: [
+        Constants.emailScope,
+      ],
+    );
+    final signInAccount = await googleSignIn.signIn();
+    print('SIGN IN $signInAccount');
+    if (signInAccount == null) {
+      return AuthResult.aborted;
+    }
+    final googleAuth = await signInAccount.authentication;
+    final oauthCredential = GoogleAuthProvider.credential(
+      idToken: googleAuth.idToken,
+      accessToken: googleAuth.accessToken,
+    );
+    try {
+      await FirebaseAuth.instance.signInWithCredential(oauthCredential);
+      return AuthResult.success;
+    } catch (e) {
+      print('Errororor $e');
+      return AuthResult.failure;
+    }
+  }
+}
